0ab73e5a56499760032e01e8c188007691bc15c9
Czy <651525974@qq.com>
2020-07-01 16:21:03 +0800

    mini-program-cli init

---
+++ Mini-Program/package/pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+        
+		<layout title="Welcome" :top="false">
+            <view class="x-center">Mini-Program-CLI</view>
+        </layout>
+        
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ Mini-Program/package/vector/dispose.js
@@ -0,0 +1,31 @@
+"use strict";
+import globalData from "@/modules/global-data";
+import {toast} from "@/modules/toast";
+import {extend} from  "@/modules/copy";
+import {Request} from "@/modules/request";
+import {PubSub} from "@/modules/event-bus";
+import {extDate} from "@/modules/datetime";
+import {checkUpdate} from  "@/modules/update";
+
+function disposeApp(app){
+    extDate(); //拓展Date原型
+    checkUpdate(); // 检查更新
+    app.$scope.toast = toast;
+    app.$scope.extend = extend;
+    app.$scope.eventBus = new PubSub();
+    app.$scope.extend(app.$scope, new Request());
+    app.$scope.extend(app.globalData, globalData);
+    app.globalData.colorN = app.globalData.colorList.length;
+}
+
+/**
+ * APP启动事件
+ */
+function onLaunch() {
+    var app = this;
+    disposeApp(this);
+    // init app data
+}
+
+
+export default {onLaunch, toast}
